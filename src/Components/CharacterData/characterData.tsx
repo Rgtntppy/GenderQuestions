754b0e43ce229f480991210d0bde20c151d264d0
NULL <rgtntppy@gmail.com>
2024-07-20 10:09:35 +0900

    Refactor: Reorganized directory structure

---
+++ src/Components/CharacterData/characterData.tsx
@@ -0,0 +1,32 @@
+import { CharacterData } from "src/Components/CharacterData/characterDataInterface";
+
+const characterDataList:CharacterData[] = [
+    // {
+    //     name: '伊達あやね',
+    //     gender: '女',
+    // },{
+    //     name: '若葉まこと',
+    //     gender: '女',
+    // },{
+    //     name: '兎鞠まり',
+    //     gender: '女',
+    // },{
+    //     name: '近所のおじさん',
+    //     gender: '男',
+    // },{
+    //     name: 'エイリアン',
+    //     gender: '不明',
+    // },
+    {
+        name: 'あいう',
+        gender: '女',
+    },{
+        name: 'アイウ',
+        gender: '女',
+    },{
+        name: 'ABC',
+        gender: '女',
+    },
+];
+
+export default characterDataList;
---
+++ src/Components/GameComponent.tsx
@@ -0,0 +1,102 @@
+import 'src/Components/gameComponent.scss';
+import React, { useState, useEffect } from 'react';
+import { CharacterData } from 'src/Components/CharacterData/characterDataInterface';
+import characterDataList from 'src/Components/CharacterData/characterData';
+import SGButton from 'src/Components/SelectGenderButton/SGButton';
+import CheckAnswer from 'src/Components/System/CheckAnswer';
+import ClickCounter from 'src/Components/System/ClickCounter';
+import Popup from 'src/Components/System/Popup';
+
+const shuffleArray = (array: CharacterData[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+const GameComponent: React.FC = () => {
+    const [ clickData, setClickData ] = useState<string>('');
+    const [ characterQueue, setCharacterQueue ] = useState<CharacterData[]>([]);
+    const [ characterItem, setCharacterItem ] = useState<CharacterData | null>(null);
+    const [ showPopup, setShowPopup ] = useState(false);
+    const [ isCorrect, setIsCorrect ] = useState(false);
+    const [ counter, setCounter ] = useState<number>(1);
+    const [ correctCounter, setCorrectCounter ] = useState<number>(0);
+
+    const questionLimit = 3;
+
+    const pushButtonData = (clickData: string) => {
+        setClickData(clickData)
+    };
+
+    const nextQuestion = () => {
+        if (characterQueue.length === 0) {
+            const shuffledCharacters = shuffleArray([...characterDataList]);
+            setCharacterQueue(shuffledCharacters);
+        }
+
+        const newCharacterItem = characterQueue[0];
+        setCharacterQueue(characterQueue.slice(1));
+        setCharacterItem(newCharacterItem);
+        
+        setClickData('');
+        setShowPopup(false);
+        setCounter(prevCounter => prevCounter + 1);
+    };
+
+    useEffect(() => {
+        if (characterDataList.length > 0) {
+            const shuffledCharacters = shuffleArray([...characterDataList]);
+            setCharacterQueue(shuffledCharacters);
+            setCounter(0);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (characterQueue.length > 0 && !characterItem) {
+            nextQuestion();
+        }
+    }, [characterQueue, characterItem]);
+
+    if (!characterItem) {
+        return <p>Loading...</p>; // データがまだ読み込まれていない場合のローディング表示
+    }
+
+    return (
+        <div className='gameScreen'>
+            <div className='questionText'>
+                <div className='questionCharacterImg'><p>ここに画像が入ります</p></div>
+                <p className='characterName'>{characterItem.name}</p>
+            </div>    
+            <SGButton clickData={ pushButtonData } showPopup={showPopup}/>
+            <CheckAnswer
+                clickData={clickData}
+                characterItem={characterItem}
+                setIsCorrect={setIsCorrect}
+                showPopup={showPopup}
+                setShowPopup={setShowPopup}
+                isCorrect={isCorrect}
+                correctCounter={correctCounter}
+                setCorrectCounter={setCorrectCounter}
+            />
+            <ClickCounter counter={counter} questionLimit={questionLimit}/>
+            {showPopup && 
+                <Popup 
+                    clickData={clickData}
+                    characterItem={characterItem}
+                    setIsCorrect={setIsCorrect}
+                    showPopup={showPopup}
+                    setShowPopup={setShowPopup}
+                    nextQuestion={nextQuestion}
+                    isCorrect={isCorrect}
+                    counter={counter}
+                    questionLimit={questionLimit}
+                    correctCounter={correctCounter}
+                />
+            }
+        </div>
+    );
+};
+
+export default GameComponent;
