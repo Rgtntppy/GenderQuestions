52b0dc9c68fe346505195766b201e6a4cabb0e51
NULL <rgtntppy@gmail.com>
2024-08-13 09:25:02 +0900

    dataのrename

--- src/Components/SelectGenderButton/SGButton.tsx
+++ src/Components/SelectGenderButton/SGButton.tsx
@@ -1,29 +1,27 @@
 import 'src/Components/SelectGenderButton/SGButton.scss';
 import { useCallback, MouseEventHandler } from "react";
-import { SGList } from 'src/Components/SelectGenderButton/SGList';
 import { ClickData } from 'src/Components/SelectGenderButton/SGInterface';
 
-const CustomButton: React.FC<ClickData> = ({ clickData, showPopup }) => {
-    const handleClick: MouseEventHandler<HTMLButtonElement>  = useCallback((event) => {
-        const index = parseInt(event.currentTarget.dataset.index || '0',10);
-        const SGItem = SGList[index];
-        console.log(SGItem.selectGender+'がクリックされました');
-        clickData(SGItem.selectGender);
+const CustomButton: React.FC<ClickData> = ({ characterItem, clickData, showPopup }) => {
+    const handleClick: MouseEventHandler<HTMLButtonElement>  = useCallback ((event) => {
+        const selectGender = event.currentTarget.dataset.select || '';
+        console.log(selectGender+'がクリックされました');
+        clickData(selectGender);
     },[]);
     
     return (
         <div className='selectButton'>
         <ul>
-            {SGList.map((SGItem, index) => (
+            {[characterItem.select1, characterItem.select2, characterItem.select3, characterItem.select4].map((selectButton, index) => (
             <button
             key={ index }
             className='customButton'
             onClick={ handleClick }
-            data-index={ index }
+            data-select={ selectButton }
             disabled={showPopup}
             >
-                <li key={ SGItem.selectGender }>
-                    { SGItem.selectGender }
+                <li key={ selectButton }>
+                    { selectButton }
                 </li>
             </button>
             ))}
--- src/Components/SelectGenderButton/SGInterface.ts
+++ src/Components/SelectGenderButton/SGInterface.ts
@@ -1,4 +1,7 @@
+import { CharacterData } from 'src/Components/CharacterData/characterDataInterface';
+
 export interface ClickData {
+    characterItem: CharacterData;
     clickData: (SGItem: string) => void;
     showPopup: boolean;
 }
