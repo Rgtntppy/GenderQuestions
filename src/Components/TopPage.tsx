cf30f0c6171916b6bf6858fe276ca7de7add908a
NULL <rgtntppy@gmail.com>
2024-09-15 13:24:44 +0900

    TopPageRenewal

--- src/Components/TopPage.tsx
+++ src/Components/TopPage.tsx
@@ -1,11 +1,14 @@
 import 'src/Components/topPage.scss';
 import { Link } from 'react-router-dom';
+import topPageImage from 'src/Components/DALL·E 2024-09-07 16.54.53 - A playful, vibrant top page image for a game that challenges users to guess the gender of people and animals. The image should feature a fun and engag.webp';
 
 const TopPage = () => {
     return (
         <div className='topPage'>
-            <h1 className='title'>gender<br></br>question</h1>
-            <div className='sk_img'><p>ここに画像が入ります</p></div>
+            <div className='titleContainer'>
+                <h1 className='title'>gender<br></br>question</h1>
+            </div>
+            <img className='sk_img' src={topPageImage}/>
             <Link to='GameComponent' className='startButton'>ゲームスタート</Link>
         </div>
     );
