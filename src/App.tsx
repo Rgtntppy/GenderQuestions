bf22a073a6b2536c4c4c7d6914b18c879c30fcad
NULL <rgtntppy@gmail.com>
2024-10-04 12:21:41 +0900

    パフォーマンス向上

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import 'src/app.scss';
-
-const TopPage = lazy(() => import('src/Components/TopPage'));
-const GameComponent = lazy(() => import('src/Components/GameComponent'));
-const ResultComponent = lazy(() => import('src/Components/Result/Result'));
+import 'app.scss';
+import TopPage from 'Components/TopPage';
+import GameComponent from 'Components/GameComponent';
+import ResultComponent from 'Components/Result/Result';
 
 const App = () => {
   return (
     <Router basename='/GenderQuestions/docs'>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<TopPage />} />
-          <Route path='/GameComponent' element={<GameComponent />} />
-          <Route path='/Result/Result' element={<ResultComponent />} />
-          <Route path='*' element={<Navigate to='/' replace />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path='/' element={<TopPage />} />
+        <Route path='/GameComponent' element={<GameComponent />} />
+        <Route path='/Result/Result' element={<ResultComponent />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
     </Router>
   );
 };
